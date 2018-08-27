0985fbc23c7fd4b397ae1c3eacff996449f68e48
Nick Maynard <nickcmaynard@users.noreply.github.com>
2018-08-27 14:48:02 +0100

    Load font from CDN - didn't embed correctly on mobile safari

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import 'hammerjs';
-import 'typeface-overpass';
 
 if (environment.production) {
   enableProdMode();
