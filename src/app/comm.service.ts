b7a3be4ba98532b9a42c38d033716dcbafec97f2
Nick Maynard <nickcmaynard@users.noreply.github.com>
2019-01-13 09:03:26 +0000

    Variable state debouncing

--- src/app/comm.service.ts
+++ src/app/comm.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ng-socket-io';
+import { Observable } from 'rxjs';
+import { debounce } from 'rxjs/operators';
+
+const actionQuietPeriod = 5000;
+const defaultDebounce = 1000;
 
 @Injectable()
 export class CommService {
 
+  static _stateDebounceTime: integer = defaultDebounce;
+  static _stateDebounceTimer: Object;
+  
   constructor(private socket: Socket) { }
 
   sendMessage(msg: string) {
@@ -17,7 +25,7 @@ export class CommService {
 
   getState() {
     return this.socket
-      .fromEvent('state');
+      .fromEvent('state').pipe(debounce(() => Observable.timer(CommService._stateDebounceTime)));
   }
 
   getProgrammeInfo() {
@@ -29,8 +37,17 @@ export class CommService {
     return this.socket
       .fromEvent('track-info');
   }
+  
+  isInQuietPeriod() {
+    return CommService._stateDebounceTime !== 0;
+  }
 
   action(type, args) {
+    // Setting an action.  Debounce state updates for a bit
+    CommService._stateDebounceTime = actionQuietPeriod;
+    clearTimeout(CommService._stateDebounceTimer);
+    CommService._stateDebounceTimer = setTimeout(() => { CommService._stateDebounceTime = defaultDebounce }, actionQuietPeriod);
+    
     return this.socket.emit(`action-${type}`, args);
   }
 }
